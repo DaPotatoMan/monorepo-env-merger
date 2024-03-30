69eec7404f0d42b63c15a0ae5e9515760ea262ec
dapotatoman <iam.fahadul@gmail.com>
2024-03-30 20:17:56 +0600

    chore: update tests

--- test/fixtures/basic/app.vue
+++ test/fixtures/basic/app.vue
@@ -1,3 +1,11 @@
+<script lang="ts" setup>
+const env = import.meta.env
+</script>
+
 <template>
-  <div>basic</div>
+  <div>
+    <template v-for="value, key in env" :key="key">
+      {{ key }} = {{ value }}
+    </template>
+  </div>
 </template>
