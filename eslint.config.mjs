ac543217a81beda52ac5916c2dfa95a86b7f0884
dapotatoman <iam.fahadul@gmail.com>
2024-03-30 13:59:48 +0600

    chore: update eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+// import withNuxt from './.nuxt/eslint.config.mjs'
+
+// export default withNuxt(
+//   antfu({
+//     // ...@antfu/eslint-config options
+//   }),
+//   // ...your other rules
+// )
+
+export default antfu()
